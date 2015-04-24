2138e89267f5e1e3fabdfb2a026ed660ad805939
Dwi Prihandi <dwi_prihandi@yahoo.com>
2015-04-24 08:33:59 +0000

    finish Following

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   
   get 'sessions/new'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   
   root 'static_pages#home'
