d50d8919576480e1cc672c55e2cffd09d419a7ee
Dwi Prihandi <dwi_prihandi@yahoo.com>
2015-05-08 03:24:41 +0000

    update Gmail Env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'gmail.com',
-  user_name:            'karir.carfix@gmail.com',
-  password:             'apxyvorscydznuto',
+  user_name:            ENV["EMAIL"],
+  password:             ENV["PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
   
